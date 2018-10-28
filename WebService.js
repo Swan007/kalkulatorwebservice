51ca747a33c5381976a9dfb0b09d8913c3fd91e3
Maulid Ihsan <maulid.ihsan@gmail.com>
2018-10-28 19:45:27 +0700

    add frontend and change port to 9090

--- WebService.js
+++ WebService.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const infixToPostfix = require('./lib/infixToPostfix');
 const calculator = require('./lib/postfixCalculator');
 const app = express();
-const port = 80;
+const port = 9090;
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.sendfile('./index.html'));
 app.post('/calculate', (req, res) => {
     console.log(req.body.formula);
     const postfix = infixToPostfix(req.body.formula);
